3f81bba139f4917a1b044c00840967695a4cebf0
Russell Chubb <Russell.d.chubb@gmail.com>
2026-04-07 20:44:33 +1200

    bugfix/ trying to get deployments working v2

--- app/app.py
+++ app/app.py
@@ -1,14 +1,16 @@
 # Golfah: Entry Point
 # This is the main entry point for the Golfah Streamlit application.
 
-# Imports
-import streamlit as st
-from utils.Navbar import navbar
-import sys  # Needed for deployment
-from pathlib import Path  # Needed for deployment
+# Deployment
+import sys
+from pathlib import Path
 
 # Needed for deployment to ensure the app can find the views and utils modules
 sys.path.insert(0, str(Path(__file__).parent))
+
+# Imports
+import streamlit as st
+from utils.Navbar import navbar
 
 # Page Setup
 st.set_page_config(page_title="Golfah", page_icon="⛳", layout="wide")
