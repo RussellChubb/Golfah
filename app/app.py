ddb8cf5d41e0c638db090d85742706c01ad54e45
Russell Chubb <Russell.d.chubb@gmail.com>
2026-04-07 20:41:28 +1200

    bugfix/ trying to get deployments working

--- app/app.py
+++ app/app.py
@@ -4,6 +4,11 @@
 # Imports
 import streamlit as st
 from utils.Navbar import navbar
+import sys  # Needed for deployment
+from pathlib import Path  # Needed for deployment
+
+# Needed for deployment to ensure the app can find the views and utils modules
+sys.path.insert(0, str(Path(__file__).parent))
 
 # Page Setup
 st.set_page_config(page_title="Golfah", page_icon="⛳", layout="wide")
